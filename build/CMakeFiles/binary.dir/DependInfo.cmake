3fc58ced3b008a7a8b507e5f4bf47cbcca0ac210
Gordon <gkamer@outlook.com>
2020-12-31 23:02:27 +0000

    build

---
+++ build/CMakeFiles/binary.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/gkamer/Desktop/Projects/GitRepos/chess/src/chess.cpp" "/Users/gkamer/Desktop/Projects/GitRepos/chess/build/CMakeFiles/binary.dir/src/chess.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  ".././src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
