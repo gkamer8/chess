58021520e42a24f0a03d0fd468b75b4aaa3774a5
Gordon <gkamer@outlook.com>
2021-01-01 00:13:32 +0000

    color choice

--- src/chess.cpp
+++ src/chess.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <stdlib.h>
+#include "chess.h"
 
 using namespace std;
 
+int cpu_color;
+
 int main(){
-    cout << "Bruh\n";
+
+    while(true){
+        char col_choice;
+        cout << "Black (b), white (w), or random (r)?\n";
+        if(cin >> col_choice){
+            if(col_choice == 'b'){
+                cpu_color = BLACK;
+                break;
+            }
+            else if(col_choice == 'w'){
+                cpu_color = WHITE;
+                break;
+            }
+            else if(col_choice == 'r'){
+                cpu_color = rand() % 2 ? WHITE : BLACK;
+                break;
+            }
+            else{
+                cout << "Selection invalid" << "\n";
+            } 
+        }
+        else{
+            cout << "Selection invalid";
+        }
+    }
+
+    cout << cpu_color << "\n";
+
+    // Main game loop
+    while(true){
+        break;
+    }
 }
---
+++ src/chess.h
@@ -0,0 +1,3 @@
+
+#define WHITE 0
+#define BLACK 1
